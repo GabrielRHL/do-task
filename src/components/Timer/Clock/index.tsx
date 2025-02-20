bcdf8f07abe1248a8aaf5b9df098725e49181120
Gabriel <gabriell-rocha@hotmail.com>
2025-02-20 16:57:11 -0300

    adicionando scss para o componente Clock

--- src/components/Timer/Clock/index.tsx
+++ src/components/Timer/Clock/index.tsx
@@ -1,11 +1,13 @@
+import style from './Clock.module.scss'
+
 function Clock() {
     return (
         <>
-            <span>0</span>
-            <span>0</span>
-            <span>:</span>
-            <span>0</span>
-            <span>0</span>
+            <span className={style.clockNumber}>0</span>
+            <span className={style.clockNumber}>0</span>
+            <span className={style.clockSplit}>:</span>
+            <span className={style.clockNumber}>0</span>
+            <span className={style.clockNumber}>0</span>
         </>
     )
 }
