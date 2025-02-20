ee10350c1008acab73f33782df878797afc0d981
Gabriel <gabriell-rocha@hotmail.com>
2025-02-20 16:38:05 -0300

    criando componente Item

---
+++ src/components/List/Item/index.tsx
@@ -0,0 +1,12 @@
+import style from '../List.module.scss'
+
+function Item({ name, time }: { name: string, time: string}) {
+    return (
+        <li className={style.item}>
+            <h3>{name}</h3>
+            <span>{time}</span>
+        </li>
+    )
+}
+
+export default Item;
