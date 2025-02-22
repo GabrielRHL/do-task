6bf3a7c25794bcf679b86540370ebe069a4586af
Gabriel <gabriell-rocha@hotmail.com>
2025-02-22 14:00:57 -0300

    aplicando state no componente Form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,20 +3,30 @@ import Button from '../Button'
 import style from './Form.module.scss'
 
 class Form extends React.Component {
+    state = {
+        name: '',
+        time: '00:00'
+    }
+
+    addTask(event: React.FormEvent) {
+        event.preventDefault()
+
+    }
+
     render() {
         return (
-            <form className={style.newTask}>
+            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor='task'>
                         Adicione um novo estudo
                     </label>
-                    <input type='text' name='task' id='task' placeholder='O que você quer estudar?' required/>
+                    <input type='text' name='task' id='task' placeholder='O que você quer estudar?' value={this.state.name} onChange={event => this.setState({...this.state, name: event.target.value})} required/>
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor='time'>
                         Tempo
                     </label>
-                    <input type='time' step='1' name='time' id='time' min='00:00:00' max='01:30:00' required/>
+                    <input type='time' step='1' name='time' id='time' value={this.state.time} onChange={event => this.setState({...this.state, time: event.target.value})} min='00:00:00' max='01:30:00' required/>
                 </div>
                 <Button>Adicionar</Button>
             </form>
