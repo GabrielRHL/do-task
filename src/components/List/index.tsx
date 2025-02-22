0af172ecf946d8ce2bfe6f6663db2e77e7e1161a
Gabriel <gabriell-rocha@hotmail.com>
2025-02-22 12:46:52 -0300

    colocando useState no componente Item

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,8 +1,9 @@
 import style from './List.module.scss'
 import Item from "./Item";
+import { useState } from 'react';
 
 function List() {
-    const tasks = [{
+    const [tasks, setTasks] = useState([{
         name: 'JavaScript',
         time: '02:00:00'
       },
@@ -10,10 +11,12 @@ function List() {
         name: 'ReactJs',
         time: '01:00:00'
       }
-    ]
+    ]);
     return (
         <aside className={style.tasksList}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTasks([...tasks, {name: 'TypeScript', time: '01:30:00'}])
+            }}>Estudos do dia</h2>
             <ul>
                 {tasks.map((task, index) => (
                     <Item key={index} {...task}/>
