fdc138939766500cc54fa26c6f869009dab2e7fa
Gabriel <gabriell-rocha@hotmail.com>
2025-02-22 14:37:10 -0300

    refatorando codigo para usar o TasksInterface

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,22 +1,11 @@
 import style from './List.module.scss'
 import Item from "./Item";
-import { useState } from 'react';
+import { TasksInterface } from '../../types/task';
 
-function List() {
-    const [tasks, setTasks] = useState([{
-        name: 'JavaScript',
-        time: '02:00:00'
-      },
-      {
-        name: 'ReactJs',
-        time: '01:00:00'
-      }
-    ]);
+function List({ tasks }: {tasks: TasksInterface[]}) {
     return (
         <aside className={style.tasksList}>
-            <h2 onClick={() => {
-                setTasks([...tasks, {name: 'TypeScript', time: '01:30:00'}])
-            }}>Estudos do dia</h2>
+            <h2>Estudos do dia</h2>
             <ul>
                 {tasks.map((task, index) => (
                     <Item key={index} {...task}/>
