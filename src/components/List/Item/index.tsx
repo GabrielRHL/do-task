b77170ae16a43e98e4538b8c26873ce8c119e341
Gabriel <gabriell-rocha@hotmail.com>
2025-02-24 13:57:08 -0300

    componente Item recebendo novas props

--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -1,6 +1,7 @@
+import { TasksInterface } from '../../../types/task';
 import style from '../List.module.scss'
 
-function Item({ name, time }: { name: string, time: string}) {
+function Item({ name, time, selected, completed, id }: TasksInterface) {
     return (
         <li className={style.item}>
             <h3>{name}</h3>
