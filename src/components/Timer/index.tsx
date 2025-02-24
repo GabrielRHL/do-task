81eb97b40720e2c1e719fa5e2da6802a562f1aee
Gabriel <gabriell-rocha@hotmail.com>
2025-02-24 17:05:03 -0300

    testando a conversao do tempo no cronometro

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import { TasksInterface } from "../../types/task";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from './Timer.module.scss'
+import { timeToSeconds } from "../../common/utils/time";
 
-function Timer() {
+interface Props {
+    selected: TasksInterface | undefined
+}
+
+function Timer({ selected }: Props) {
+    const [time, setTime] = useState<number>()
+    if(selected?.time) {
+        setTime(timeToSeconds(selected.time))
+    }
     return (
         <div className={style.timer}>
             <p className={style.title}>Escolha um card e inicie o temporizador</p>
+            Tempo: {time}
             <div className={style.clockWrapper}>
                 <Clock />
             </div>
