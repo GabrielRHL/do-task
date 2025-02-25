0efb877e56a5fd5fcd18b9c7082cc2b44e33d41b
Gabriel <gabriell-rocha@hotmail.com>
2025-02-25 16:15:01 -0300

    finalizando funcionalidade do cronometro mostrar o tempo da tarefa

--- src/components/Timer/Clock/index.tsx
+++ src/components/Timer/Clock/index.tsx
@@ -1,13 +1,21 @@
 import style from './Clock.module.scss'
 
-function Clock() {
+interface Props {
+    time: number | undefined
+}
+
+function Clock({ time = 0 }: Props) {
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
+    const [minTens, minOnes] = String(minutes).padStart(2, '0')
+    const [segTens, segOnes] = String(seconds).padStart(2, '0')
     return (
         <>
-            <span className={style.clockNumber}>0</span>
-            <span className={style.clockNumber}>0</span>
+            <span className={style.clockNumber}>{minTens}</span>
+            <span className={style.clockNumber}>{minOnes}</span>
             <span className={style.clockSplit}>:</span>
-            <span className={style.clockNumber}>0</span>
-            <span className={style.clockNumber}>0</span>
+            <span className={style.clockNumber}>{segTens}</span>
+            <span className={style.clockNumber}>{segOnes}</span>
         </>
     )
 }
