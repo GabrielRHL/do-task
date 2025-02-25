1362b075acdce8a90912de6e504f6eb268532168
Gabriel <gabriell-rocha@hotmail.com>
2025-02-25 17:13:35 -0300

    refatorando codigo

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import style from './Button.module.scss'
 
-interface ButtonProps extends React.PropsWithChildren<{}> {
+interface Props {
     type?: 'button' | 'submit' | 'reset' | undefined, 
-    onClick?: () => void
+    onClick?: () => void,
+    children?: React.ReactNode
 }
-class Button extends React.Component<ButtonProps> {
-    render() {
-        const { type = 'button', onClick } = this.props
-        return (
-            <button onClick={onClick} type={type} className={style.button}>
-                {this.props.children}
-            </button>
-        )
-    }
+
+function Button({ onClick, type, children }: Props) {
+    return (
+        <button onClick={onClick} type={type} className={style.button}>
+            {children}
+        </button>
+    )
 }
 
 export default Button;
